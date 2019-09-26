0cd80540cd4f6a37090ba25009d23e3ccb908dcd
gechenchen <gechenchen@peilian.com>
2019-09-26 15:22:07 +0800

    项目初始化。。。

---
+++ src/main/java/com/haonan/mowel/MowelApplication.java
@@ -0,0 +1,13 @@
+package com.haonan.mowel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MowelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MowelApplication.class, args);
+    }
+
+}
